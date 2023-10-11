65dffb9eb2d3a8521a15a69db9e30d4908112f3e
kktlove <kkt09072@gmail.com>
2023-10-11 12:32:58 +0900

    springframework BCryptPasswordEncoder member

---
+++ pro04_2/src/main/java/kr/ed/haebeop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package kr.ed.haebeop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "/index";
+    }
+}
